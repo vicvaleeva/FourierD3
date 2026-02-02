fix stress calculation and add CN correction

src/fastd3/calculator.py
@@ -54,7 +54,26 @@ class FastD3ASECalculator(Calculator):
         self._model = None
 
     def _update_cell(self, cell):
-            self._model._update_cell(cell)
+            self._model._update_cell(cell=cell)
+            
+    def _update_cndiff(self, atoms, large_rcut = 20.0):
+        cell = torch.tensor(atoms.cell.array, dtype=torch.float32, device=self.device)
+        positions = torch.tensor(
+            atoms.positions,
+            dtype=torch.float32,
+            device=self.device,
+            requires_grad=False,
+        )
+        
+        edge_index, unit_shifts = self._build_graph(atoms, large_rcut)
+        shifts = torch.matmul(unit_shifts, cell)
+        cn_large = self._model.compute_cn(positions * self.angstrom_to_bohr, edge_index, shifts * self.angstrom_to_bohr, recalc=True)
+        
+        edge_index, unit_shifts = self._build_graph(atoms)
+        shifts = torch.matmul(unit_shifts, cell)
+        cn_small = self._model.compute_cn(positions * self.angstrom_to_bohr, edge_index, shifts * self.angstrom_to_bohr, recalc=True)
+        
+        self._model._update_cndiff(cndiff=(cn_large - cn_small))
 
     def _build_model(self, atoms):
         self._model = FastD3(
@@ -73,13 +92,16 @@ class FastD3ASECalculator(Calculator):
         )
 
     # ideally this reuse nlist from the MLIP but for now let's keep it this for benchmarking
-    def _build_graph(self, atoms):
+    def _build_graph(self, atoms, rcut = None):
+        if rcut is None:
+            rcut = self.r_cut
+            
         sender, receiver, unit_shifts = neighbour_list(
             quantities="ijS",
             pbc=atoms.pbc,
             cell=atoms.cell,
             positions=atoms.positions,
-            cutoff=self.r_cut,
+            cutoff=rcut,
         )
 
         edge_index = torch.tensor(
@@ -139,7 +161,7 @@ class FastD3ASECalculator(Calculator):
         forces = -positions.grad
         stress = (
             strain.grad
-            / self._model.volume
+            / self._model.volume * (self.angstrom_to_bohr ** 3)
         )
 
         # -------------------------

src/fastd3/core.py
@@ -36,16 +36,21 @@ class FastD3(torch.nn.Module):
         method: str = 'spme',
         interpolation_nodes: int = 4,
         k_cutoff: float = 10.0,
+        cndiff: Optional[torch.tensor] = None,
         verbose = True
     ) -> None:
         super().__init__()
         
         self.device = device
         self.method = method
-        #
         self.mesh_spacing = mesh_spacing
         self.interpolation_nodes = interpolation_nodes
         self.k_cutoff = k_cutoff
+        
+        if cndiff is not None:
+            self.cndiff = cndiff.to(device)
+        else:
+            self.cndiff = None
 
         if pbc is not None:
             assert pbc.all(), "particle-mesh only supports 3d pbc"
@@ -164,10 +169,16 @@ class FastD3(torch.nn.Module):
             self.kvectors = generate_kvectors_for_ewald(ns=self.ns, cell=self.cell)
             self.knorm = torch.linalg.norm(self.kvectors, dim=1)
             self.G = self.potential.lr_from_k_sq(self.knorm)
+            
+    def _update_cndiff(self, cndiff):
+        if cndiff is not None:
+            self.cndiff = cndiff.to(self.device)
+        else:
+            self.cndiff = None
 
     @torch.jit.export
     def compute_cn(self, positions: torch.Tensor, edge_index: torch.Tensor, 
-                   shifts: torch.Tensor) -> torch.Tensor:
+                   shifts: torch.Tensor, recalc = False) -> torch.Tensor:
         """Compute coordination numbers with fused operations."""
         positions = positions.to(dtype=torch.float32)
         n_atoms = positions.size(0)
@@ -200,6 +211,9 @@ class FastD3(torch.nn.Module):
         # Scatter add
         cn = torch.zeros(n_atoms, device=self.device, dtype=torch.float32)
         cn.index_add_(0, source_m, edge_contributions)
+        
+        if self.cndiff is not None and not recalc:
+            cn += self.cndiff
         
         return cn
     