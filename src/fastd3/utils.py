fc39058bd82c079334950e570e18057b8e363dc2
Victoria Valeeva <vicky.valeeva@gmail.com>
2026-02-09 12:14:40 -0800

    add seed for eigendecomposition

--- src/fastd3/utils.py
+++ src/fastd3/utils.py
@@ -16,13 +16,15 @@ def maxrel_err(ref, approx) -> float:
     mask = np.where(ref == 0, 1.0, ref)
     return np.max(np.abs(ref-approx)*np.where(ref == 0, 0, 1/mask))
 
-def decomp(types: List, c6tol: float, verbose: bool):
+def decomp(types: List, c6tol: float, verbose: bool, seed: int = 42):
     c6ref_mat = load_c6ref(types)
+    rng = np.random.default_rng(seed)
+    v0 = rng.standard_normal(c6ref_mat.shape[0])
     k = 1
-    eigs, eigvecs = eigsh(c6ref_mat, k=k)
+    eigs, eigvecs = eigsh(c6ref_mat, k=k, v0=v0)
     while maxrel_err(c6ref_mat, eigvecs @ np.diag(eigs) @ eigvecs.T)*100 >= c6tol:
         k += 1
-        eigs, eigvecs = eigsh(c6ref_mat, k=k)
+        eigs, eigvecs = eigsh(c6ref_mat, k=k, v0=v0)
     err = maxrel_err(c6ref_mat, eigvecs @ np.diag(eigs) @ eigvecs.T)
     if verbose:
         print(f'Using {k}-rank decomposition with maximum relative error: {err*100} %')   
